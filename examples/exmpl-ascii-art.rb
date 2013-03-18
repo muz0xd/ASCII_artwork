2feb490c3e0c4261de1a396d89e80340f2c90e2d
DrunkenElk <drunken_elk@2ch.hk>
2013-03-19 00:19:03 +0400

    Method to draw a line Method to draw a circle Method for scaling Random background

--- examples/exmpl-ascii-art.rb
+++ examples/exmpl-ascii-art.rb
@@ -1,1 +1,22 @@
 require '../lib/ASCII_artwork'
+
+cvs = Canvas.new 61, 61, ' '
+
+cvs.line_set({x: 30, y: 60}, {x:  0, y: 30}, "#")
+cvs.line_set({x: 30, y: 60}, {x: 20, y: 30}, '#')
+cvs.line_set({x: 30, y: 60}, {x: 40, y: 30}, '#')
+cvs.line_set({x: 30, y: 60}, {x: 60, y: 30}, '#')
+
+cvs.line_set({x: 0, y: 30}, {x: 60, y: 30}, '#')
+
+cvs.line_set({x:  0, y: 30}, {x: 10, y: 15}, "#")
+cvs.line_set({x: 20, y: 30}, {x: 10, y: 15}, '#')
+cvs.line_set({x: 20, y: 30}, {x: 30, y: 15}, '#')
+cvs.line_set({x: 40, y: 30}, {x: 30, y: 15}, '#')
+cvs.line_set({x: 40, y: 30}, {x: 50, y: 15}, '#')
+cvs.line_set({x: 60, y: 30}, {x: 50, y: 15}, '#')
+
+cvs.line_set({x: 10, y: 15}, {x: 50, y: 15}, '#')
+
+cvs.p_console
+cvs.scale!(10).p_file("ruby.bmp", {foreground: "0000ff"})
--- lib/ASCII_artwork/Canvas.rb
+++ lib/ASCII_artwork/Canvas.rb
@@ -1,63 +1,138 @@
 class Canvas
+  BACKGROUND_COLORS = [
+    "FF0000",
+    "00FF00",
+    "0000FF",
+    "FFFF00",
+    "00FFFF",
+    "FF00FF",
+    "C0C0C0",
+    "FFFFFF"
+  ]
+
   def initialize(width, height, background)
     @width, @height, @background = width, height, background
     @data_matrix = Array.new(height) { Array.new(width) {background} }
   end
-  
+
   def cell_set(x, y, symbol = '.', clean = false)
     if clean then symbol = @background end
     if (0...@width) === x && (0...@height) === y
       @data_matrix[y][x] = symbol
     end
   end
- 
-  def line_set (k0 = {x: nil, y: nil}, k1 ={x: nil, y: nil} ,
-                  symbol = '@', clean = false)
-    
-    if k0.include?(nil) || k1.include?(nil) || k0.length != 2 || k1.length != 2
-      return
-    end
-    
-    steep = (k1[:y] - k0[:y]).abs > (k1[:x] - k0[:x]).abs
-    if steep
-      k0[:x], k0[:y] = k0[:y], k0[:x]
-      k1[:x], k1[:y] = k1[:y], k1[:x]
-    end
-    if k0[:x] > k1[:x]
-      k0[:x], k1[:x] = k1[:x], k0[:x]
-      k0[:y], k1[:y] = k1[:y], k0[:y]
-    end
-    deltax = k1[:x] - k0[:x]
-    deltay = (k1[:y] - k0[:y]).abs
-    error = (deltax/2).to_i
-    y = k0[:y]
-    k0[:y] < k1[:y] ? ystep = 1 : ystep = -1
-    (k0[:x]..k1[:x]).each do |x|
-      if clean
-        steep ? cell_set(y, x, symbol, true) : cell_set(x, y, symbol, true)
-      else
-        steep ? cell_set(y, x, symbol) : cell_set(x, y, symbol)
+
+  def line_set (point_0 = {x: nil, y: nil},
+                point_1 = {x: nil, y: nil},
+                symbol  = '@', clean = false)
+
+    x0, y0, x1, y1 = point_0[:x], point_0[:y], point_1[:x], point_1[:y]
+    return unless x0 && y0 && x1 && y1
+
+    delta_x = (x1 - x0).abs
+    delta_y = (y1 - y0).abs
+    sign_x  = x0 < x1 ? 1 : -1
+    sign_y  = y0 < y1 ? 1 : -1
+
+    error = delta_x - delta_y
+
+    cell_set(x1, y1, symbol, clean)
+    while(x0 != x1 || y0 != y1)
+      cell_set(x0, y0, symbol, clean)
+      tmp_error = error * 2
+
+      if tmp_error > -delta_y
+        error -= delta_y
+        x0 += sign_x
       end
-      error -= deltay
-      if error < 0
-        y += ystep
-        error += deltax
+
+      if tmp_error < delta_x
+        error += delta_x
+        y0 += sign_y
       end
     end
   end
-  
+
+  def circle_set(point = {x: nil, y: nil}, radius = nil, symbol = '@', clean = false)
+    x0, y0 = point[:x], point[:y]
+    return unless x0 && y0 && radius
+
+    x = 0
+    y = radius
+    delta = 2 - 2 * radius
+    error = 0
+
+    while(y >= 0)
+      cell_set(x0 + x, y0 + y, symbol, clean)
+      cell_set(x0 + x, y0 - y, symbol, clean)
+      cell_set(x0 - x, y0 + y, symbol, clean)
+      cell_set(x0 - x, y0 - y, symbol, clean)
+
+      error = 2 * (delta + y) - 1
+      if(delta < 0 && error <= 0)
+        x += 1
+        delta += 2 * x + 1
+        next
+      end
+
+      error = 2 * (delta - x) - 1
+      if(delta > 0 && error > 0)
+        y -= 1
+        delta += 1 - 2 * y
+        next
+      end
+
+      x += 1
+      delta += 2 * (x - y)
+      y -= 1
+    end
+  end
+
+  def scale!(scale = 1)
+    old_data_matrix = @data_matrix
+
+    @width = @width * scale
+    @height = @height * scale
+    @data_matrix = Array.new(@height) { Array.new(@width) {@background} }
+
+    @data_matrix.each_with_index do |line, i|
+      line.each_with_index do |symbol, j|
+        original_i = i / scale
+        original_j = j / scale
+        original_symbol = old_data_matrix[original_i][original_j]
+        cell_set(j, i, original_symbol)
+      end
+    end
+    self
+  end
+
+  def random_background_color
+    BACKGROUND_COLORS[rand(BACKGROUND_COLORS.count)]
+  end
+
   def p_console
     @data_matrix.each do |line|
       line.each {|symbol| print symbol}
       puts
     end
   end
-  
-  def p_file(filename)
+
+  def p_file(filename, options = {})
+    default_options = {foreground: "000000", background: "ffffff", random: false}
+    options = default_options.merge(options)
+
     bmp = BMP::Writer.new(@width, @height)
     @data_matrix.each_with_index do |line, i|
       line.each_with_index do |symbol, j|
-        bmp[j, i] = symbol == @background ? "ffffff" : "000000"
+        bmp[j, i] = if symbol == @background
+          if options[:random]
+            random_background_color
+          else
+            options[:background]
+          end
+        else
+          options[:foreground]
+        end
       end
     end
     bmp.save_as(filename)
